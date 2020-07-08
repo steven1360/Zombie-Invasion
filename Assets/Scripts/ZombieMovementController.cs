f5e12fadf4f89977b55e37204f35a358af198362
q-q-q-q-q <truongsteven107@gmail.com>
2020-07-07 21:16:33 -0700

    zombie AI

--- Assets/Scripts/ZombieMovementController.cs
+++ Assets/Scripts/ZombieMovementController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum WandererBehavior_SM { Start, GoToRandomLocation, Wait, Chase}
+
+
 
 
 public class ZombieMovementController : MonoBehaviour
@@ -10,32 +13,27 @@ public class ZombieMovementController : MonoBehaviour
     [SerializeField] private ZombieStatManager statController;
     private Rigidbody2D rb;
     private Animator anim;
+    private ZombieVisionRange range;
+    private WandererBehavior_SM wanderer_state;
+    private Vector2 randomDestination;
+    private Clock clock;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        Random.InitState(System.DateTime.Now.Millisecond);
         rb = transform.root.GetComponent<Rigidbody2D>();
         anim = transform.root.GetComponent<Animator>();
+        range = transform.GetChild(0).GetComponent<ZombieVisionRange>();
+        clock = new Clock(3.5f, 0f);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        float dy = Mathf.Abs(player.position.y - transform.position.y);
-        float dx = Mathf.Abs(player.position.x - transform.position.x);
-        bool farEnoughAwayFromPlayer = Mathf.Pow(dx, 2) + Mathf.Pow(dy, 2) > 3.9f;
-
-
-        Vector2 zombieToPlayer = (player.position - transform.position).normalized;
-        LookAt(player.position);
-
-        if (farEnoughAwayFromPlayer)
-        {
-            rb.MovePosition(transform.root.position + (Vector3)zombieToPlayer * statController.Stats.Speed * Time.deltaTime);
-            anim.SetBool("Moving", true);
-        }
-
-        
+        Tick_WandererSM();
     }
     
     void LookAt(Vector2 targetToLookAt)
@@ -61,6 +59,99 @@ public class ZombieMovementController : MonoBehaviour
         }
 
         transform.parent.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+
+
+    void Tick_WandererSM()
+    {
+        switch (wanderer_state)
+        {
+            case WandererBehavior_SM.Start:
+                anim.SetBool("Moving", true);
+                randomDestination = GetRandomPosition();
+                LookAt(randomDestination);
+                wanderer_state = WandererBehavior_SM.GoToRandomLocation;
+                break;
+            case WandererBehavior_SM.GoToRandomLocation:
+                bool arrivedAtTarget = GoToTarget(randomDestination, 0.1f);
+                if (arrivedAtTarget)
+                {
+                    wanderer_state = WandererBehavior_SM.Wait;
+                    anim.SetBool("Moving", false);
+                }
+                if (range.PlayerInRange)
+                {
+                    wanderer_state = WandererBehavior_SM.Chase;
+                }
+                break;
+            case WandererBehavior_SM.Wait:
+                if (clock.ReachedDesiredWaitTime())
+                {
+                    wanderer_state = WandererBehavior_SM.GoToRandomLocation;
+                    randomDestination = GetRandomPosition();
+                    LookAt(randomDestination);
+                    clock.ResetClock();
+                    anim.SetBool("Moving", true);
+                }
+                else
+                {
+                    clock.Tick(Time.fixedDeltaTime);
+                }
+
+                if (range.PlayerInRange)
+                {
+                    wanderer_state = WandererBehavior_SM.Chase;
+                }
+                break;
+            case WandererBehavior_SM.Chase:
+                if (!range.PlayerInRange) wanderer_state = WandererBehavior_SM.Wait;
+                ChasePlayer();
+
+                break;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        float dy = Mathf.Abs(player.position.y - transform.position.y);
+        float dx = Mathf.Abs(player.position.x - transform.position.x);
+        bool farEnoughAwayFromPlayer = Mathf.Pow(dx, 2) + Mathf.Pow(dy, 2) > 3.9f;
+        Vector2 zombieToPlayer = (player.position - transform.position).normalized;
+
+        if (farEnoughAwayFromPlayer && range.PlayerInRange)
+        {
+            rb.MovePosition(transform.root.position + (Vector3)zombieToPlayer * statController.Stats.Speed * Time.deltaTime);
+            LookAt(player.position);
+            anim.SetBool("Moving", true);
+        }
+        else
+        {
+            anim.SetBool("Moving", false);
+        }
+    }
+
+    bool GoToTarget(Vector3 target, float stoppingDistance)
+    {
+        float dy = Mathf.Abs(target.y - transform.position.y);
+        float dx = Mathf.Abs(target.x - transform.position.x);
+        Vector2 direction = (target - transform.position).normalized;
+        bool notWithinStoppingDistance = Mathf.Pow(dx, 2) + Mathf.Pow(dy, 2) >= Mathf.Pow(stoppingDistance, 2);
+        //Debug.Log("distance :" + Mathf.Pow(dx, 2) + Mathf.Pow(dy, 2));
+        if (notWithinStoppingDistance)
+        {
+            rb.MovePosition(transform.root.position + (Vector3)direction * statController.Stats.Speed * Time.deltaTime);
+            return false;
+        }
+        return true;
+
+    }
+
+    Vector2 GetRandomPosition()
+    {
+        float dx = Random.Range(-4f, 4f);
+        float dy = Random.Range(-4f, 4f);
+        return new Vector2(transform.position.x + dx, transform.position.y + dy);
     }
 
 }
---
+++ Assets/Scripts/ZombieVisionRange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieVisionRange : MonoBehaviour
+{
+    public bool PlayerInRange { get; private set; }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            PlayerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            PlayerInRange = false;
+        }
+    }
+}
